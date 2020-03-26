5e11a210474deaecc96e5367c405bd191d528d5a
Chigozie Duruobioha <61623639+chigolo1@users.noreply.github.com>
2020-03-26 17:45:31 +0100

    Add files via upload

---
+++ firstpage.js
@@ -0,0 +1,15 @@
+
+let name="chigozie";
+let data=[];
+const courses=["html","css","javascript","kotlin","java"];
+console.log("my name is :"+ name)
+console.log(courses)
+for (let i = 0; i < 100; i++) {
+    if(i%2!==0 && i < 200){
+        data.push(i)
+    }
+    
+}
+
+console.log(data);    
+    
